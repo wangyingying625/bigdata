18037965e0c76958ddae221372af87d327b6d5b9
taropowder <31129902+taropowder@users.noreply.github.com>
2018-09-08 10:40:43 +0800

    Update tool.php

--- upload/tool.php
+++ upload/tool.php
@@ -84,7 +84,7 @@ include_once "connect.php";
                     <tbody>
                     <?php
                     include_once "connect.php";
-                    $dir="./upload/file_upload/tool/";
+                    $dir="file_upload/tool/";
                     $sql = "SELECT * FROM file_type_tool";
                     $result = mysqli_query($connect, $sql);
                     while(@$data=mysqli_fetch_array($result))
