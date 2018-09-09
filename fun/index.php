08dd3f2eecf8fded27a22e3835ae9d99281c5eec
wyy <1224374496@qq.com>
2018-09-09 19:21:31 +0800

    删了很多重复的静态文件，别再在旧的上面改啦，别再在旧的上面改啦，别再在旧的上面改啦！！！

--- fun/index.php
+++ fun/index.php
@@ -1,6 +1,36 @@
 <?php
-include 'base.php';
+session_start();
+include_once "./about_db/print_list.php";
+include_once "connect.php";
 ?>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>休闲娱乐</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="../font-awesome-4.7.0/css/font-awesome.min.css">
+        <link href="../css/fonts.googleapis.css" rel="stylesheet">
+        <script src="../js/new.js"></script>
+        <link href="../css/googleapls.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="../css/prettyPhoto.css" />
+        <link href="../css/jquery.bsPhotoGallery.css" rel="stylesheet">
+
+        <link rel="stylesheet" type="text/css" href="../static/h-ui/css/H-ui.min.css" />
+        <link rel="stylesheet" type="text/css" href="../static/h-ui.admin/css/H-ui.admin.css" />
+        <link rel="stylesheet" type="text/css" href="../lib/Hui-iconfont/1.0.8/iconfont.css" />
+        <link rel="stylesheet" type="text/css" href="../static/h-ui.admin/skin/default/skin.css" id="skin" />
+        <link rel="stylesheet" type="text/css" href="../static/h-ui.admin/css/style.css" />
+        <link rel="stylesheet" type="text/css" href="../upload/css/style.css">
+        <link rel="stylesheet" type="text/css" href="../upload/css/new.css">
+        <!--[if IE 6]>
+        <script type="text/javascript" src="../lib/DD_belatedPNG_0.0.8a-min.js"></script>
+        <script>DD_belatedPNG.fix('*');</script>
+        <![endif]-->
+    </head>
+
 
 <body>
 <header>
